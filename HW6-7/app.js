c9da0628e9fae7bd965d02a5890eb4b67bdddd4d
Karmyshev Abiy <99629706+KarnaAbiy@users.noreply.github.com>
2023-01-28 18:57:07 +0600

    Add files via upload

---
+++ HW6-7/app.js
@@ -0,0 +1,6 @@
+var div = document.getElementById('color');
+var r = Math.floor(Math.random() * 256);
+    g = Math.floor(Math.random() * 256);
+    b = Math.floor(Math.random() * 256);
+
+div.style.backgroundColor = "rgb("+r+","+g+","+b+")";
